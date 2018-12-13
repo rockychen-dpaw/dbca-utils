fc8cadf504986e1c8029d9ab3450b6db25e4c651
Ashley Felton <ashley@ropable.com>
2018-12-13 13:01:54 +0800

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dbca-utils',
+    version='1.0.0',
+    description='Utilities for Django/Python apps',
+    url='https://github.com/dbca-wa/dpaw-utils',
+    author='Department of Biodiversity, Conservation and Attractions',
+    author_email='asi@dbca.wa.gov.au',
+    license='Apache License, Version 2.0',
+    packages=['dbca_utils'],
+    zip_safe=False)
