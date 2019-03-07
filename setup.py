96e5b112ce5b276978393162597011b8b6eb1dd6
Ashley Felton <ashley@ropable.com>
2019-03-07 08:23:33 +0800

    Update setup.py with include_package.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='dbca-utils',
-    version='1.0.0',
+    version='1.0.1',
+    packages=find_packages(),
     description='Utilities for Django/Python apps',
     url='https://github.com/dbca-wa/dpaw-utils',
     author='Department of Biodiversity, Conservation and Attractions',
     author_email='asi@dbca.wa.gov.au',
     license='Apache License, Version 2.0',
     packages=['dbca_utils'],
-    zip_safe=False)
+    zip_safe=False,
+    install_requires=[
+        'requests',
+        'six',
+    ]
+)
