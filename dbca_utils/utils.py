7fd121badf540d028db3cd2c15c799dea340ee03
Ashley Felton <ashley@ropable.com>
2020-02-06 08:29:14 +0800

    Formatting fixes.

--- dbca_utils/utils.py
+++ dbca_utils/utils.py
@@ -1,9 +1,9 @@
-__version__ = '1.0.0'
 import ast
 import os
+__version__ = '1.0.0'
 
 
-def env(key, default=None, required=False,value_type=None):
+def env(key, default=None, required=False, value_type=None):
     """
     Retrieves environment variables and returns Python natives. The (optional)
     default will be returned if the environment variable does not exist.
@@ -24,10 +24,10 @@ def env(key, default=None, required=False,value_type=None):
 
     if value_type is None:
         return value
-    elif isinstance(value,value_type):
+    elif isinstance(value, value_type):
         return value
-    elif issubclass(value_type,list):
-        if isinstance(value,tuple):
+    elif issubclass(value_type, list):
+        if isinstance(value, tuple):
             return list(value)
         else:
             value = str(value).strip()
@@ -35,8 +35,8 @@ def env(key, default=None, required=False,value_type=None):
                 return []
             else:
                 return value.split(",")
-    elif issubclass(value_type,tuple):
-        if isinstance(value,list):
+    elif issubclass(value_type, tuple):
+        if isinstance(value, list):
             return tuple(value)
         else:
             value = str(value).strip()
@@ -44,7 +44,7 @@ def env(key, default=None, required=False,value_type=None):
                 return tuple()
             else:
                 return tuple(value.split(","))
-    elif issubclass(value_type,bool):
+    elif issubclass(value_type, bool):
         value = str(value).strip()
         if not value:
             return False
@@ -53,13 +53,10 @@ def env(key, default=None, required=False,value_type=None):
         elif value.lower() == 'false':
             return False
         else:
-            raise Exception("'{}' is a boolean environment variable, only accept value 'true' ,'false' and '' with case insensitive, but the configured value is '{}'".format(key,value))
-    elif issubclass(value_type,int):
+            raise Exception("'{}' is a boolean environment variable, only accept value 'true' ,'false' and '' with case insensitive, but the configured value is '{}'".format(key, value))
+    elif issubclass(value_type, int):
         return int(value)
-    elif issubclass(value_type,float):
+    elif issubclass(value_type, float):
         return float(value)
     else:
-        raise Exception("'{0}' is a {1} environment variable, but {1} is not supported now".format(key,value_type))
-
-
-
+        raise Exception("'{0}' is a {1} environment variable, but {1} is not supported now".format(key, value_type))
