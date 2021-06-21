17077d443629a78cd21fd72a97ce0ab4fb471eb1
Ashley Felton <ashley@ropable.com>
2021-06-22 07:48:13 +0800

    Remove unused imports, reformat.

--- dbca_utils/models.py
+++ dbca_utils/models.py
@@ -1,15 +1,11 @@
-from __future__ import unicode_literals
 from django.conf import settings
-from django.contrib.auth import get_user_model
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils import timezone
-import threading
 
 
 class ActiveMixinManager(models.Manager):
-    """Manager class for ActiveMixin.
-    """
+    """Manager class for ActiveMixin."""
+
     def current(self):
         return self.filter(effective_to=None)
 
@@ -24,6 +20,7 @@ class ActiveMixin(models.Model):
 
     "effective_to" is used to flag 'deleted' objects (not null==deleted).
     """
+
     effective_to = models.DateTimeField(null=True, blank=True)
     objects = ActiveMixinManager()
 
@@ -40,8 +37,8 @@ class ActiveMixin(models.Model):
         """Overide the standard delete method; sets effective_to the current
         date and time.
         """
-        if 'force' in kwargs and kwargs['force']:
-            kwargs.pop('force', None)
+        if "force" in kwargs and kwargs["force"]:
+            kwargs.pop("force", None)
             super(ActiveMixin, self).delete(*args, **kwargs)
         else:
             self.effective_to = timezone.now()
@@ -52,12 +49,23 @@ class AuditMixin(models.Model):
     """Model mixin to update creation/modification datestamp and user
     automatically on save.
     """
+
     creator = models.ForeignKey(
-        settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.PROTECT,
-        related_name='%(app_label)s_%(class)s_created', editable=False)
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT,
+        related_name="%(app_label)s_%(class)s_created",
+        editable=False,
+    )
     modifier = models.ForeignKey(
-        settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.PROTECT,
-        related_name='%(app_label)s_%(class)s_modified', editable=False)
+        settings.AUTH_USER_MODEL,
+        blank=True,
+        null=True,
+        on_delete=models.PROTECT,
+        related_name="%(app_label)s_%(class)s_modified",
+        editable=False,
+    )
     created = models.DateTimeField(default=timezone.now, editable=False)
     modified = models.DateTimeField(auto_now=True, editable=False)
 
@@ -72,8 +80,7 @@ class AuditMixin(models.Model):
                 self._initial[field.attname] = getattr(self, field.attname)
 
     def has_changed(self):
-        """Returns True if the current data object differs from saved.
-        """
+        """Returns True if the current data object differs from saved."""
         return bool(self.changed_data)
 
     @property
